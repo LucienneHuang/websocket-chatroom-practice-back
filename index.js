79fc4c6dc1db28987aaf1143ea816278b1c63993
q22641949@gmail.com <q22641949@gmail.com>
2025-02-24 23:35:41 +0800

    feat: 加入、發送訊息

---
+++ index.js
@@ -0,0 +1,35 @@
+import WebSocket, { WebSocketServer } from "ws";
+const wss = new WebSocketServer({
+    port: 4000
+});
+const clients = new Map();
+wss.on('connection', (ws, req) => {
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        if (!data.user) {
+            ws.send(JSON.stringify({ error: "缺少使用者名稱" }));
+            return;
+        }
+        clients.set(ws, data.user);
+        if(data.type === 'join') {
+            broadcast({ text: `${data.user} 加入了聊天室`});
+        } else if(data.type === 'message') {
+            broadcast(data);
+        }
+    });
+    ws.on('error', console.error);
+    ws.on("close", () => {
+        const user = clients.get(ws);
+        if(user) {
+            broadcast({ text: `${user} 離開了聊天室`});
+            clients.delete(ws);
+        }
+    });
+})
+const broadcast = (data) => {
+    clients.forEach((_, client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(data));
+        }
+    });
+}
